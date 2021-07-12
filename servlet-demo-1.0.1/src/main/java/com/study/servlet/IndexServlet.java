5e19bca103a6bcd10789af6a8d845f892bbda014
justin7160PC <justin7160@gmail.com>
2021-07-12 14:11:40 +0800

    servlet-demo-1.0.1 for TomcatServer-Demo

---
+++ servlet-demo-1.0.1/src/main/java/com/study/servlet/IndexServlet.java
@@ -0,0 +1,30 @@
+package com.study.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class IndexServlet extends HttpServlet {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public void init() throws ServletException {
+		System.out.println("servlet-demo IndexServlet init .........");
+	}
+
+
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		System.out.println("######## DoGet Override ########");
+	}
+
+	
+}
